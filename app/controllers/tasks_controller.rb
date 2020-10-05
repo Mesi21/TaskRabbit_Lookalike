0c1ca49761100e4188f79c1429a3ecb657eb6d27
Mesi21 <mse212002@gmail.com>
2020-10-05 17:09:22 +0300

    :sparkles: Add categories to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,12 @@ class TasksController < ApplicationController
   before_action :find_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = Task.all.order('created_at DESC')
+    if params[:category].blank?
+			@tasks = Task.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@tasks = Task.where(category_id: @category_id).order("created_at DESC")
+		end
   end
 
   def show; end
@@ -41,7 +46,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :company, :url)
+    params.require(:task).permit(:title, :description, :company, :url, :category_id)
   end
 
   def find_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
-
 class Task < ApplicationRecord
+    belongs_to :category
 end
