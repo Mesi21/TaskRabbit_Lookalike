2a8128b7b46136c35e5c76249fecc6eaa3807375
Molnar Emese <mse212002@gmail.com>
2020-10-05 13:06:38 +0300

    :sparkles: added edit, update, show and destroy actions to controller and added the view file for edit action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,9 +27,16 @@ class TasksController < ApplicationController
     end
 
     def update
+        if @task.update(task_params)
+            redirect_to @task
+        else
+            render 'Edit'
+        end
     end
     
     def destroy
+        @task.destroy
+        redirect_to root_path
     end
 
     private
